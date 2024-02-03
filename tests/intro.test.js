cde8bc3397622444d66c470ecc72b0f5ef139ebc
enes-dogan <enesdogan210@gmail.com>
2024-02-03 18:49:06 +0300

    testing fizzBuzz fn

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,6 @@
 import { describe, test, it, expect } from 'vitest';
 import { max } from '../src/intro';
+import { fizzBuzz } from '../src/intro';
 
 describe('max', () => {
   it('should return the first argument if it is greater', () => {
@@ -12,5 +13,23 @@ describe('max', () => {
 
   it('should return the first argument if arguments are equal', () => {
     expect(max(1, 1)).toBe(1);
+  });
+});
+
+describe('fizzBuzz', () => {
+  it('should return "FizzBuzz" if argument divisible by 3 and 5', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  it('should return "Fizz" if argument only divisible by 3', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  });
+
+  it('should return "Buzz" if argument only divisible by 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it('should return argument as string if argument is not divisible by 3 and 5', () => {
+    expect(fizzBuzz(2)).toBe('2');
   });
 });
