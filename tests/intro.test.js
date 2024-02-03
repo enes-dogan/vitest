52a31da43f8d24125cd4f14b0857e2a337a13b2b
enes-dogan <enesdogan210@gmail.com>
2024-02-03 18:39:32 +0300

    testing max() function

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,3 +1,16 @@
 import { describe, test, it, expect } from 'vitest';
+import { max } from '../src/intro';
 
-describe('', () => {});
+describe('max', () => {
+  it('should return the first argument if it is greater', () => {
+    expect(max(2, 1)).toBe(2);
+  });
+
+  it('should return the second argument if it is greater', () => {
+    expect(max(2, 3)).toBe(3);
+  });
+
+  it('should return the first argument if arguments are equal', () => {
+    expect(max(1, 1)).toBe(1);
+  });
+});
